f9c5965a0e4c52e09c01758eeb02e08a09f2057c
George Ivanov <georgei@accretivetg.com>
2013-10-27 23:33:46 -0700

    Test

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,3 @@
 exports.index = function(req, res){
-  res.render('index');
+  res.render('index', {debug:true, title:'George first page is here', response : { name : 'Response name is rendered!!', value: 11}});
 };
