38139b15e1d37bed1d6ebe81a7a330113a92d475
George Ivanov <georgei@accretivetg.com>
2013-10-29 23:02:16 -0700

    Changing the company name to Nasko company

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,13 @@
-exports.index = function(req, res){
-  res.render('index', {debug:true, title:'George first page is here', response : { name : 'Response name is rendered!!', value: 11}});
+exports.index = function (req, res) {
+    res.render('index',
+        {
+            debug: true,
+            company: {
+                title: 'Дезинфект Сливен',
+                name: '<<Company name here',
+                company_slogan : 'Борба с гризачи и насекоми. Ефикасно премахване на хребарки, бълхи, кърлежи, мухи, мишки, плъхове',
+                company_description: 'Високо качество. Работи с висококачествени и ефикасни препарати (BASF, Gingenta). Безплатен оглед и консулация на обекти. 20 години опит с доволни клиенти и безупречни услуги'
+            }
+        }
+    );
 };
